aba922103248ef3b05f09a28fcaf7b646e393516
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-08-07 20:35:12 -0700

    Assingment Done

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 $(function() {
   // your code here
+      $('.accordion-header').on('click',function(){
+        // Below will only allow to open one p
+        $('.accordion-header').not(this).next('div').slideUp()
+        $(this).next('div').slideToggle()
+    })
+
+    $('.todos button').on('click', function(){
+      $.ajax({
+        url:"https://dummyjson.com/todos",
+        type:"GET",
+        success: function(response){
+          const todos = response.todos
+          buildTodos(todos)
+        },
+        error: function(err){
+          console.log(err)
+        }
+      })
+    })
 })
+
+function buildTodos(arr){
+  console.log(arr)
+  const todoList = $('.todos ul')
+  arr.forEach(element => {
+    const li = $('<li></li>')
+    li.text(`${element.todo}`)
+    todoList.append(li)
+  });
+
+}
